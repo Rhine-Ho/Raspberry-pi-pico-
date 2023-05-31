e327dff7c24b57112ce1d48a687c9f47f100267f
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-05-31 23:39:59 +0800

    Create PWM_control_led.py

---
+++ LED/PWM_control_led.py
@@ -0,0 +1,12 @@
+from machine import Pin, PWM
+from time import sleep
+
+pwm = PWM(Pin(25))
+pwm.freq(50)
+
+while True:
+    for duty in range (1000, 20000, 1):
+        pwm.duty_u16(duty)
+        sleep(0.0001)
+    sleep(0.3)
+     
